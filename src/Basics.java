daa75111424e9b12f0391b202f196ceaff741499
20gaurav04 <btechug2026@gmail.com>
2024-09-18 21:24:59 +0530

    new commit

--- src/Basics.java
+++ src/Basics.java
@@ -2,9 +2,40 @@ import java.util.Scanner;
 
 public class Basics{
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        System.out.print("Enter the value of X :");
-        int x = input.nextInt();
-        System.out.print("The value of X is : " + x);
+
+//        if-else conditions
+//        Scanner sc = new Scanner(System.in);
+//        System.out.print("Enter a number:");
+//        int n = sc.nextInt();
+//        if(n>100)
+//        {
+//            System.out.println("The number is greater than 100");
+//        }
+//        else
+//        {
+//            System.out.println("The number is less than 100");
+//        }
+
+
+//        while loop & for loops
+
+//        Scanner val = new Scanner(System.in);
+//        System.out.println("Enter value : ");
+//        int value = val.nextInt();
+//        while(value != 100)
+//        {
+//            System.out.println("Val =" + value);
+//            value = value +1;
+//
+//        }
+
+//        System.out.print("Enter the number whose table you wannt to print : ");
+//        Scanner sc = new Scanner(System.in);
+//        int n = sc.nextInt();
+//
+//        for(int i = 1 ; i<=10 ; i++)
+//        {
+//            System.out.println(n + "*" + i + "=" + n*i);
+//        }
     }
 }
