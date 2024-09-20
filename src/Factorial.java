04d9c8059d3d4f5a1d52e21f2deb6c8b094e9ee7
20gaurav04 <btechug2026@gmail.com>
2024-09-20 21:57:34 +0530

    new commit

--- src/Factorial.java
+++ src/Factorial.java
@@ -1,22 +1,23 @@
 import java.util.Scanner;
 
-public class Factorial{
-    public static void main(String[] args) {
+public class Factorial {
+    public static void main(String [] args){
+        System.out.println("Enter the val:");
         Scanner sc = new Scanner(System.in);
-        System.out.print("Enter the number :");
         int x = sc.nextInt();
 
-        int fact =1;
-
-        if(x==0 || x==1) {
-            System.out.print("The factorial of 0 is : 1");
+        int fact = 1;
+        if(x == 0 || x==1)
+        {
+            System.out.print("The factorial of"+" "+x+"is :1");
         }
-        else {
-            for(int i = 1;i<=x;i++)
+        else{
+            for(int i=1;i<=x;i++)
             {
-                fact=fact*i;
+                fact = fact*i;
+
             }
-            System.out.print("The factorial of"+x+"is"+ " "+fact);
         }
+        System.out.print("the fact is"+fact);
     }
 }
